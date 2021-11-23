ed79a62a6ef3f52db5b056770cddfc83b80b75d3
Beni <benimadhab11@gmail.com>
2021-11-23 22:24:41 +0530

    Project setup

---
+++ src/test/java/com/zooplus/coding/task/TaskApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zooplus.coding.task;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TaskApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
